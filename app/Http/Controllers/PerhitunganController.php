20a6ca40165ad2299eb63197e1882fd07cbfe9bb
farisqlail <faris.riskilail@gmail.com>
2021-12-07 16:36:52 +0700

    group by

--- app/Http/Controllers/PerhitunganController.php
+++ app/Http/Controllers/PerhitunganController.php
@@ -23,24 +23,20 @@ class PerhitunganController extends Controller
         $kriteria = Kriteria::where('id_lowongan', $id)->get();
         $alternatif = Pelamar::where('id_lowongan', $id)->get();
         $kode_krit = [];
-        foreach ($kriteria as $krit)
-        {
+        foreach ($kriteria as $krit) {
             $kode_krit[$krit->id_kriteria] = [];
-            foreach ($alternatif as $al)
-            {
-                foreach ($al->bobot as $bobot)
-                {
-                        if ($bobot->kriteria->id_kriteria == $krit->id_kriteria)
-                        {
-                            $kode_krit[$krit->id_kriteria][] = $bobot->jumlah_bobot;
-                        }
+            foreach ($alternatif as $al) {
+                foreach ($al->bobot as $bobot) {
+                    if ($bobot->kriteria->id_kriteria == $krit->id_kriteria) {
+                        $kode_krit[$krit->id_kriteria][] = $bobot->jumlah_bobot;
+                    }
                 }
             }
 
-            if ($krit->atribut_kriteria == 'cost' && !empty($kode_krit[$krit->id_kriteria])){
+            if ($krit->atribut_kriteria == 'cost' && !empty($kode_krit[$krit->id_kriteria])) {
 
                 $kode_krit[$krit->id_kriteria] = min($kode_krit[$krit->id_kriteria]);
-            } elseif ($krit->atribut_kriteria == 'benefit' && !empty($kode_krit[$krit->id_kriteria])){
+            } elseif ($krit->atribut_kriteria == 'benefit' && !empty($kode_krit[$krit->id_kriteria])) {
 
                 $kode_krit[$krit->id_kriteria] = max($kode_krit[$krit->id_kriteria]);
             } else {
@@ -48,35 +44,44 @@ class PerhitunganController extends Controller
                 $kode_krit[$krit->id_kriteria] = 1;
             }
         };
-//        return json_encode($kode_krit);
-        return view('perhitungan.index',[
+        //        return json_encode($kode_krit);
+        return view('perhitungan.index', [
             'kriteria'      => $kriteria,
             'alternatif'    => $alternatif,
             'kode_krit'     => $kode_krit
         ]);
     }
 
-    public function perhitungan2(){
+    public function perhitungan2($id)
+    {
+
+        $lowongan = Lowongan::all();
+        $lowonganId = $lowongan[0]->id_lowongan;
 
         $daftarSoal = DaftarSoal::all();
         $daftarSoalGet = $daftarSoal[0]->id_soal;
-        $hasilTes = HasilTes::join('daftar_soal', 'daftar_soal.id_soal', '=', 'hasil_tes.id_soal_tes')->where('hasil_tes.id_soal_tes', $daftarSoalGet)->get();
- 
-    
+        $hasilTes = HasilTes::join('daftar_soal', 'daftar_soal.id_soal', '=', 'hasil_tes.id_soal_tes')
+            ->where('hasil_tes.id_soal_tes', '=', $daftarSoalGet)
+            ->get();
+
+        // dd($hasilTes);   
+
         return view('perhitungan.seleksi2', [
             'daftarSoal' => $daftarSoal,
             'hasilTes' => $hasilTes
         ]);
     }
 
-    public function lowongan(){
+    public function lowongan()
+    {
 
         $lowongan = lowongan::all();
 
         return view('perhitungan.lowongan', ['lowongan' => $lowongan]);
     }
 
-    public function detail($id){
+    public function detail($id)
+    {
 
         $pelamar = Pelamar::find($id);
 
